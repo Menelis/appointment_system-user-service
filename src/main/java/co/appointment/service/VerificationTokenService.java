d835e2ce1aa517c4f527325598d51f26c0d2e876
Menelisi Mthembu <menelismthembu@gmail.com>
2025-10-27 22:41:39 +0200

    Set token and expiryDate on VerificationTokenService

--- src/main/java/co/appointment/service/VerificationTokenService.java
+++ src/main/java/co/appointment/service/VerificationTokenService.java
@@ -1,22 +1,32 @@
 package co.appointment.service;
 
+import co.appointment.config.AppConfigProperties;
 import co.appointment.entity.VerificationToken;
 import co.appointment.repository.VerificationTokenRepository;
 import co.appointment.util.ObjectUtils;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class VerificationTokenService {
 
     private final VerificationTokenRepository verificationTokenRepository;
+    private final AppConfigProperties.VerificationTokenSettings verificationTokenSettings;
 
+    public VerificationTokenService(final VerificationTokenRepository verificationTokenRepository, final AppConfigProperties appConfigProperties) {
+        this.verificationTokenRepository = verificationTokenRepository;
+        this.verificationTokenSettings = appConfigProperties.getVerificationToken();
+    }
     public VerificationToken createVerificationToken(final VerificationToken verificationToken) {
+        verificationToken.setToken(UUID.randomUUID().toString());
+        verificationToken.setExpiryDate(ObjectUtils.addMillisecondsToCurrentDate(verificationTokenSettings.getExpirationMs()).getTime());
+        if(verificationToken.getId() != null) {
+            verificationToken.setCreatedAt(LocalDateTime.now());
+        }
         return verificationTokenRepository.save(verificationToken);
     }
     public VerificationToken getVerificationToken(final String token) {
