3174c54e9a9f221dc76413c08d3bca871726b90c
Menelisi Mthembu <menelismthembu@gmail.com>
2025-10-27 22:44:01 +0200

    Update reference of TokenConstants after moving it to shared library

--- src/main/java/co/appointment/security/jwt/AuthTokenFilter.java
+++ src/main/java/co/appointment/security/jwt/AuthTokenFilter.java
@@ -1,6 +1,6 @@
 package co.appointment.security.jwt;
 
-import co.appointment.constant.TokenConstants;
+import co.appointment.shared.constant.TokenConstants;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
