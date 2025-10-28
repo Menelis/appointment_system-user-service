7fa55d7c764cd95904bbe795ee40d293844be5d5
Menelisi Mthembu <menelismthembu@gmail.com>
2025-10-29 01:15:17 +0200

    Removed validation patterns

--- src/main/java/co/appointment/payload/request/SignUpRequest.java
+++ src/main/java/co/appointment/payload/request/SignUpRequest.java
@@ -22,12 +22,10 @@ public class SignUpRequest {
     private String email;
 
     @Size(max = 20, message = "Contact Number cannot exists 20 characters")
-    @Pattern(message = "Only numbers allowed for contact number", regexp = "^\\d{20}$")
     private String contactNo;
 
     @NotBlank(message = "Password cannot be blank")
     @NotEmpty(message = "Password cannot be empty")
     @Size(max = 20, message = "Password cannot exceed 20 characters")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password must be alphanumeric")
     private String password;
 }
