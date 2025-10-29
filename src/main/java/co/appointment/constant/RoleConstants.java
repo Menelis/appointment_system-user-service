0308052060084bdbfea7cc8835e404c7b05a85a0
Menelisi Mthembu <menelismthembu@gmail.com>
2025-10-29 23:25:01 +0200

    Delete RoleConstants.java and created them on shared library for sharing between services

--- src/main/java/co/appointment/constant/RoleConstants.java
+++
@@ -1,7 +0,0 @@
-package co.appointment.constant;
-
-public interface RoleConstants {
-    String CUSTOMER_ROLE = "CUSTOMER";
-    String USER_ROLE = "USER";
-    String ADMIN_ROLE = "ADMINISTRATOR";
-}
